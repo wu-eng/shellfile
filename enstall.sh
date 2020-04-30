eb283f3494df484a6d18408fc14d1e2928f1d92f
wu-eng <2243980708@qq.com>
2020-04-30 11:22:33 +0800

    /shellfile/enstall.sh

---
+++ enstall.sh
@@ -0,0 +1,12 @@
+#!/bin/dash
+echo -e "\033[33m-------------------------\033[0m"
+echo -e "\033[32mplease select menu follow\033[0m"
+echo -e "\033[33m-------------------------\033[0m"
+
+echo "1)安装apache服务器"
+
+echo "2)安装mysql服务器"
+
+echo "3)安装PHP服务器"
+
+echo "4)配置LAMP WEB架构"
